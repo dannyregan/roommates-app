623372dfe29acd9fc422c0f71a472444e03748ad
Danny Regan <d.regan@columbia.edu>
2025-07-20 10:03:59 -0400

    Removes template. Fresh start.

--- template-typescript-bottom-tabs-supabase-auth-flow/src/initSupabase.ts
+++ template-typescript-bottom-tabs-supabase-auth-flow/src/initSupabase.ts
@@ -1,7 +1,8 @@
 import AsyncStorage from "@react-native-async-storage/async-storage";
 import { createClient } from "@supabase/supabase-js";
+import { SUPABASE_URL, SUPABASE_ANON_KEY } from '/';
 
-// Better put your these secret keys in .env file
+// Better put your secret keys in .env file
 export const supabase = createClient("supabaseUrl", "supabaseKey", {
   localStorage: AsyncStorage as any,
   detectSessionInUrl: false // Prevents Supabase from evaluating window.location.href, breaking mobile
